0ece93a639b53f030f22e03a075d3707a7eb8823
Dil Sombayeva <dilyasom@gmail.com>
2019-05-15 19:37:12 -0700

    impletent toggle in TodoList

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -4,11 +4,11 @@ import React from 'react';
 import Todo from './Todo';
 
 const TodoList = props => {
- //  console.log('props in todolist: ', props);
+ console.log('props in todolist: ', props);
  return (
   <div>
    {props.listProps.map((item, i) => (
-    <div key={i}>
+    <div key={i} onClick={() => props.toggleProps(item.id)}>
      <Todo item={item} />
     </div>
    ))}
